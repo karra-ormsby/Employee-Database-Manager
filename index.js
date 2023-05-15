f22a62ef85e55c4129bace080656fcfc0cf56b4a
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-05-15 21:47:29 +1000

    fixed issue with add employee function

--- index.js
+++ index.js
@@ -274,25 +274,63 @@ function updateEmployee () {
         for (let i = 0; i < result.length; i++) {
             employeeArray.push(result[i].first_name + " " + result[i].last_name);
         }
+
+        db.query(`SELECT job_title FROM roles`, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+
+            for (let i = 0; i < result.length; i++) {
+                roleArray.push(result[i].job_title);
+            }
+        
+            inquirer
+                .prompt(questions)
+                .then((answer) => {
+                    let employeeId;
+                    let roleId;
+
+                    db.query(`SELECT * FROM employees`, function (err, result) {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        for (let i = 0; i < result.length; i++) {
+                            if (answer.employee === result[i].first_name + " " + result[i].last_name) {
+                                employeeId = result[i].id
+                            }
+                        }
+                    });
+
+                    db.query(`SELECT * FROM roles`, function (err, result) {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        for (let i = 0; i < result.length; i++) {
+                            if (answer.role === result[i].job_title) {
+                                roleId = result[i].id;
+                            }
+                        }
+                        console.log(roleId);
+
+                        const sql = "UPDATE employees SET role_id = ? WHERE id = ?";
+                        const values = [roleId, employeeId];
+
+                        db.query(sql, values, function (err, result) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            console.log(`Added ${answer.role} to the database`);
+                            displayEmployees();
+                        });
+                    });
+                });
+        });
+
     });
 
-    db.query(`SELECT job_title FROM roles`, function (err, result) {
-        if (err) {
-            console.log(err);
-        }
-
-        for (let i = 0; i < result.length; i++) {
-            roleArray.push(result[i].job_title);
-        }
-    });
-
-    //first question there to wait for arrays to populate
     const questions = [
-        {
-            type: 'input',
-            name: 'firstName',
-            message: "What is the employee's first name?"
-        },
         {
             type: 'list',
             message: "Which employee's role do you want to update?",
@@ -306,47 +344,4 @@ function updateEmployee () {
             choices: roleArray
         }
     ];
-
-    inquirer
-        .prompt(questions)
-        .then((answer) => {
-            let employeeId;
-            let roleId;
-
-            db.query(`SELECT * FROM employees`, function (err, result) {
-                if (err) {
-                    console.log(err);
-                }
-
-                for (let i = 0; i < result.length; i++) {
-                    if (answer.employee === result[i].first_name + " " + result[i].last_name) {
-                        employeeId = result[i].id
-                    }
-                }
-            });
-
-            db.query(`SELECT * FROM roles`, function (err, result) {
-                if (err) {
-                    console.log(err);
-                }
-
-                for (let i = 0; i < result.length; i++) {
-                    if (answer.role === result[i].job_title) {
-                        roleId = result[i].id;
-                    }
-                }
-                console.log(roleId);
-
-                const sql = "UPDATE employees SET role_id = ? WHERE id = ?";
-                const values = [roleId, employeeId];
-
-                db.query(sql, values, function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.log(`Added ${answer.role} to the database`);
-                    displayEmployees();
-                });
-            });
-        });
 }
